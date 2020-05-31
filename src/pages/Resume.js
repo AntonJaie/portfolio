6b10c02e3ee53e85d0a8ba62d538bf7cbcf20021
AntonJaie <antonjaie.dejoya@gmail.com>
2020-06-01 00:00:55 +0800

    amend changes

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,10 +1,6 @@
 import React, { forwardRef } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Box from '@material-ui/core/Box';
-import Typography from '@material-ui/core/Typography';
-import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
-import 'react-vertical-timeline-component/style.min.css';
-import WorkIcon from '@material-ui/icons/Work';
 import Aboutme from './../components/Aboutme'
 import WorkHistory from './../components/WorkHistory'
 
