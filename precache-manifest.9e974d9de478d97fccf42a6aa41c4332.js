a6809890290c9d68ae64616b7dc139b3b8f76e1d
AntonJaie <antonjaie.dejoya@gmail.com>
2020-05-31 23:51:58 +0800

    Updates

---
+++ precache-manifest.9e974d9de478d97fccf42a6aa41c4332.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45a15714fd86b6d7ca6b7252b1226696",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "174799ca6ea0fc9b9c23",
+    "url": "/react-gh-pages/static/css/2.f681e662.chunk.css"
+  },
+  {
+    "revision": "0721b734f8b1a6bfbf67",
+    "url": "/react-gh-pages/static/css/main.d0ac3433.chunk.css"
+  },
+  {
+    "revision": "174799ca6ea0fc9b9c23",
+    "url": "/react-gh-pages/static/js/2.bc344a75.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/react-gh-pages/static/js/2.bc344a75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0721b734f8b1a6bfbf67",
+    "url": "/react-gh-pages/static/js/main.65032960.chunk.js"
+  },
+  {
+    "revision": "ba0444895cc73b78ffcb",
+    "url": "/react-gh-pages/static/js/runtime-main.565a7c55.js"
+  },
+  {
+    "revision": "e24af3562a0361ea013fabec80a4856b",
+    "url": "/react-gh-pages/static/media/bg.e24af356.jpg"
+  },
+  {
+    "revision": "7d136c7998ce08e8721f292fe933b3a2",
+    "url": "/react-gh-pages/static/media/profile.7d136c79.jpg"
+  }
+]);
